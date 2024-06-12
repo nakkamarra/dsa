bd5448ec0284fb13c43819db7856c99d4a4683c5
Nick Brandt <nickbrandt64@gmail.com>
2024-06-11 23:57:05 -0400

    WIP: add delete to bst

--- bst/bst.go
+++ bst/bst.go
@@ -15,6 +15,31 @@ type Node[T constraints.Ordered] struct {
 	Left, Right *Node[T]
 }
 
+// New returns the root node of a tree with nil subtrees,
+// and a specified value.
+func New[T constraints.Ordered](value T) *Node[T] {
+	return &Node[T]{
+		Value: value,
+	}
+}
+
+// NewFrom returns the root node of a tree built using the
+// values slice as inputs.
+func NewFrom[T constraints.Ordered](values []T) *Node[T] {
+	if len(values) < 1 {
+		return nil
+	}
+	root := New[T](values[0])
+	for i := 1; i < len(values); i++ {
+		root.Insert(values[i])
+	}
+	return root
+}
+
+// Insert attempts to insert a value into the binary search tree
+// at the node from which it is called, returning an error if the
+// value is already present at the current node or in one of its
+// subtrees.
 func (n *Node[T]) Insert(value T) error {
 	if n == nil {
 		n = &Node[T]{
@@ -31,6 +56,9 @@ func (n *Node[T]) Insert(value T) error {
 	return ErrValuePresent
 }
 
+// Search queries the binary search tree for a given value,
+// returning a reference to the node containing that value if it
+// is found, or an error if no node with that value exists.
 func (n *Node[T]) Search(query T) (*Node[T], error) {
 	if n == nil {
 		return nil, ErrNotPresent
@@ -45,4 +73,31 @@ func (n *Node[T]) Search(query T) (*Node[T], error) {
 		return n.Right.Search(query)
 	}
 	return nil, ErrNotPresent
+}
+
+// Delete will remove a node with the given value from the
+// binary search tree, readjusting children / parent values
+// if necessary
+func (n *Node[T]) Delete(value T) error {
+	if n == nil {
+		return ErrNotPresent
+	}
+	if n.Value == value {
+		if n.Left == nil && n.Right == nil {
+			n = nil
+			return nil
+		}
+		return nil
+	}
+	if n.Value > value {
+		return n.Left.Delete(value)
+	}
+	if n.Value < value {
+		return n.Right.Delete(value)
+	}
+	return ErrNotPresent
+}
+
+func (n *Node[T]) Rebalance() {
+
 }
